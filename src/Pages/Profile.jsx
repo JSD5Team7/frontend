b0f6ac54f30ad7ff15c19571ebbcf760fcb7795e
Chakrit <chakritkp@hotmail.com>
2023-10-24 15:37:21 +0700

    edit config

--- src/Pages/Profile.jsx
+++ src/Pages/Profile.jsx
@@ -5,12 +5,7 @@ import Sky from "../assets/images/sky.jpg";
 
 
 const UserProfile = () => {
-<<<<<<< HEAD
   const [isEditing, setIsEditing] = useState(true);
-=======
-  const { user , updateUser ,usersData } = useAPI();
-  const [isEditing, setIsEditing] = useState(false);
->>>>>>> 0e84ca7 (add icon)
   const [profileData, setProfileData] = useState({
     firstName: "Tom ",
     lastName: "Cruise",
@@ -21,7 +16,6 @@ const UserProfile = () => {
     height: "175",
     Birtdate: "30/01/1998"
   });
-<<<<<<< HEAD
   
   console.log(isEditing);
   console.log(profileData);
@@ -40,62 +34,6 @@ const UserProfile = () => {
               className="border-slate-400 drop-shadow-lg h-40 w-40 rounded-full ml-10  "
               src="https://www.setforset.com/cdn/shop/articles/full_body_workout_on_machines_2000x.jpg?v=1660263618"
               alt="ProfileImage"
-=======
-
-const [fname , setFname] = useState(user.fname)
-const [lname , setLname] = useState(user.lname)
-  
-  // console.log(user)
-
-  const handleEditClick = () => {
-
-    // setIsEditing(!isEditing);
-  };
-
-  const handleInputChange = (e) => {
-    const { name, value } = e.target;
-    setProfileData({ ...profileData, [name]: value });
-  };
-
-
-
-  const saveButtonClass = isEditing
-  
-    ? 'block bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded'
-    : 'hidden';
-
-  const editButtonClass = isEditing
-    ? 'hidden'
-    : 'block bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded';
-
-  return (
-    <Layout>
-      <div className="flex-col justify-center gap-10">
-        <img
-          className="drop-shadow-lg h-40 w-40 rounded-full m-auto"
-          src="https://www.setforset.com/cdn/shop/articles/full_body_workout_on_machines_2000x.jpg?v=1660263618"
-          alt=""
-        />
-      </div>
-      <div>
-        <h1 className="text-center m-auto font-bold text-2xl">MY PROFILE</h1>
-        <p className='text-center'>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quas exercitationem libero asperiores.
-        </p>
-      </div>
-      <div className="p-4 w-2/3 mx-auto">
-        <form onClick={handleEditClick}>
-          <div className="mb-4">
-            <label htmlFor="firstName">First Name:</label>
-            <input
-              type="text"
-              id="firstName"
-              name="firstName"
-              // value={user.fname}
-              onChange={(e) => setFname(e.target.value)}
-              disabled={!isEditing}
-              className="input-field rounded-md"
->>>>>>> 0e84ca7 (add icon)
             />
             <p className="ml-5 text-slate-700 font-semibold text-4xl ">
               {" "}
@@ -105,7 +43,6 @@ const [lname , setLname] = useState(user.lname)
           </div>
         </div>
 
-<<<<<<< HEAD
        
         <div className="p-5 w-full mt-6 mx-auto text-center bg-slate-700 border-indigo-200 rounded-md drop-shadow">
             <p className="text-2xl text-white font-semibold" >My Profle</p>
@@ -116,20 +53,6 @@ const [lname , setLname] = useState(user.lname)
           <p className="text-left text-xl font-semibold text-white">Height (cm) :  <span className="text-lg text-white font-medium">{profileData.height}</span></p>
           <button className="font-bold text-slate-800 border-solid border-2 border-lime-200 rounded-full p-1  drop-shadow-md w-20 bg-lime-300 hover:bg-lime-400" 
                   onClick={() => setIsEditing(!isEditing)}>Edit </button>
-=======
-          <div className="mb-4">
-            <label htmlFor="lastName">Last Name:</label>
-            <input
-              type="text"
-              id="lastName"
-              name="lastName"
-              // value={user.lname}
-              onChange={(e) => setLname(e.target.value)}
-              disabled={!isEditing}
-              className="input-field rounded-md"
-            />
-          </div>
->>>>>>> 0e84ca7 (add icon)
 
 {!isEditing ? (
           <form action="" className=" w-full my-3 flex justify-around ">
@@ -254,7 +177,6 @@ const [lname , setLname] = useState(user.lname)
                   </div>
                   </div>
 
-<<<<<<< HEAD
                   <br></br>
                   <button className="font-bold text-slate-800 border-solid border-2 border-lime-200 rounded-full p-1  drop-shadow-md w-20 bg-lime-300 hover:bg-lime-400">Update</button>
             </div>
@@ -263,19 +185,6 @@ const [lname , setLname] = useState(user.lname)
         </div>
 
         
-=======
-          <button
-            type="button"
-            className={editButtonClass}
-          >
-            Edit
-          </button>
-          <button 
-          type="submit" className={saveButtonClass}>
-            Save
-          </button>
-        </form>
->>>>>>> 0e84ca7 (add icon)
       </div>
     </Layout>
   );
