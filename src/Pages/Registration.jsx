4a36f656ca6201b04a725b1246777390b8b934c2
Chakrit <chakritkp@hotmail.com>
2023-10-25 16:15:51 +0700

    add resiponsive

--- src/Pages/Registration.jsx
+++ src/Pages/Registration.jsx
@@ -1,19 +1,20 @@
 import React from "react";
 import Layout from "../Layout/Layout";
 import FormRegistration from "../Components/FormRegistration";
+import SignBanner from '../assets/images/sign-up-photocut.jpg'
 
 const Registration = () => {
 
   return (
     <Layout>
-      <div className="w-full h-full m-auto">
-        <h1 className="text-center text-6xl font-bold p-5">
+      <div className="flex flex-col h-fit justify-center items-center mb-10 xl:w-[1200px] m-auto xl:min-w-[600px] ">
+        <h1 className="text-center text-6xl font-bold p-5 mb-10">
           Welcome to Groot Club!
         </h1>
-          <div className="form-signup w-full flex items-center gap-3 mt-5 bg-emerald-200 bg-opacity-10 shadow-md shadow-black rounded-lg">
-            <div className="signup-image w-2/4 order-1">
+          <div className="form-signup w-[600px] flex items-center gap-3 mt-5 bg-emerald-200 bg-opacity-10 shadow-md shadow-black rounded-lg xl:w-[1200px]">
+            <div className="signup-image order-1 w-0 invisible xl:w-[600px] xl:visible">
               <img
-                src="/src/assets/images/sign-up-photo.jpeg"
+                src={SignBanner}
                 alt="Image signup page"
                 className="rounded-e-lg contrast-125"
               />
