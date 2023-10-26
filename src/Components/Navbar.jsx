9245f86a48c7d867a5ec51edcaf616d66701ef20
Chakrit <chakritkp@hotmail.com>
2023-10-26 11:03:40 +0700

    recheck 3 edit width img profile

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -142,10 +142,13 @@ const Navbar = ({ children }) => {
           <ul className="flex w-[150px] justify-end items-center gap-3 xl:min-w-[300px]">
             <li className="flex-col w-[40px] h-8 justify-center items-center">
               <NavLink to={"/profile"}>
+                <div className="min-h-[40px] min-w-[40px] relative overflow-hidden rounded-full">
                 <img
                   src={userImg}
-                  className="drop-shadow-lg bg-slate-100 h-[40px] w-[40px] rounded-full"
+                  className="drop-shadow-lg bg-slate-100 w-full rounded-full"
                 />
+                </div>
+               
                 {/* <FontAwesomeIcon
                 icon={faCircleUser}
                 size="xl"
